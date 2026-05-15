d7c499de19145a37c76c0f80dbdb3755a0012c31
goyal-nandini <nandinigoyal1981@gmail.com>
2026-05-15 16:59:38 +0530

    updated db credentials

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 import "dotenv/config";
 import pg from "pg";
-const {Pool} = pg;
+const { Pool } = pg;
 
 const db = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false } 
+    : false
 });
 
 export default db;
